a50c294b07dd4da24e3a6c84d6efab3e349fb6e7
mariuszt <mariuszt@rite.us>
2022-05-09 22:45:49 +0200

    first MVC

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public String Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
